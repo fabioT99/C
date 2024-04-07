63a5b7d87a354a3c2611762d27eef10871ba718a
fabioT99 <turcofabio99@gmail.com>
2024-04-07 19:16:53 +0100

    07-04-24

---
+++ C/C++L1/Calculation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int value1 = 24;
+    int value2 = 5;
+    float mul2 = 3.5;
+    int resultsum = value1 + value2;
+    float totalvalue = resultsum * mul2;
+    std::cout << "The result of the sum is: " << resultsum << std::endl;
+    std::cout <<"The result of the multiplication is: " << totalvalue << std::endl;
+    return 0;
+}
